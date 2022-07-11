cd16834b4148b58dddb2ba5209fbcc744b19f625
peppapighs <ptk.prasertsuk@gmail.com>
2022-07-11 16:19:54 +0800

    :tada: initialize frontend

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      Hello World
+    </div>
+  )
+}
+
+export default Home
